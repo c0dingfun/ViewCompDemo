d7b75f4346db68511bf392bb4fa7ec26dff0646e
c0dingfun <hackerrank888@gmail.com>
2020-03-20 12:50:40 -0700

    config logging to use nlog

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace ViewCompDemo
 {
@@ -13,6 +14,9 @@ namespace ViewCompDemo
     {
         public static void Main(string[] args)
         {
+            // using NLOG
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,7 +24,38 @@ namespace ViewCompDemo
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseStartup<Startup>()
+
+                        // configure logging and use NLOG
+                        .ConfigureLogging(logging => {
+                            logging.ClearProviders();
+                            logging.SetMinimumLevel(LogLevel.Trace);
+                        })
+                        .UseNLog();
                 });
+
+
+        // - Configure logging programmatically, via Configuration API - to use NLOG
+        // public static void Main(string[] args)
+        // {
+        //     var config = new LoggingConfiguration();    // instantiate the Logging Configuration
+            
+        //     // targets
+        //     var fileTarget = new FileTarget("fileTarget")   // NLOG's targets
+        //     {
+        //         FileName = @"c:\logs\mylog-${shortdate}.log",
+        //         Layout = "${longdate}|${event-properties:item=EventId_Id}|${uppercase:${level}}|${logger}|${message} ${exception:format=tostring}"
+        //     };
+        //     config.AddTarget(fileTarget);
+
+        //     // rules                                        // NLOG's rules
+        //     config.AddRuleForOneLevel(NLog.LogLevel.Warn, fileTarget); 
+        //     config.AddRuleForOneLevel(NLog.LogLevel.Error, fileTarget);
+        //     config.AddRuleForOneLevel(NLog.LogLevel.Fatal, fileTarget);  
+        //     LogManager.Configuration = config;
+        //     CreateWebHostBuilder(args).Build().Run();
+        // }
+        // -
     }
 }
